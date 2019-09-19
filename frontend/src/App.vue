95abc2ddf50aa45eba0fccc6997fa5b65b1cb8f0
Aaron Schweig <webdesign.schweig@gmail.com>
2019-09-19 13:15:16 +0200

    Fixed your  stuff ;-)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-    
     <div class="navbar">
-        <router-link class="title" onclick="window.location.href='home'">NHL Stats</router-link>
-        <div class="items">
-            <router-link class="item" to='home' active>Home</router-link>
-            <router-link class="item" to='demo'>Demo</router-link>
-            <router-link class="item" to='doku'>Doku</router-link>
-            <router-link class="item" to='github'>GitHub</router-link>
-        </div>
+      <router-link class="title" to="home">NHL Stats</router-link>
+      <div class="items">
+        <router-link class="item" to="home" active>Home</router-link>
+        <router-link class="item" to="demo">Demo</router-link>
+        <router-link class="item" to="doku">Doku</router-link>
+        <router-link class="item" to="github">GitHub</router-link>
+      </div>
     </div>
     <router-view></router-view>
   </div>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,21 +1,27 @@
 <template>
-    <div>
-        <div class="landing">
-            <h1>Advanced Databases</h1>
-            <h2>Cassandra</h2>
-        </div>
-        <div class="paragraph">
-            <h1>Erklärung</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione suscipit animi expedita aliquam incidunt veniam, soluta quibusdam dolor <a href="#">Hallo klick mich an</a> dignissimos quisquam, ex quo provident vero at commodi? Perferendis velit ducimus maxime! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil porro ipsum commodi provident deleniti eaque quasi iusto adipisci, molestias et delectus laborum quidem eius sunt molestiae autem harum impedit nemo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ut vel cum dolorum porro, esse labore possimus? Reiciendis sit, quae explicabo repudiandae expedita maiores, accusamus, velit autem quas facilis illo?</p>
-        </div>
-        <olap-filter title="Test" active="true"/>
+  <div>
+    <div class="landing">
+      <h1>Advanced Databases</h1>
+      <h2>Cassandra</h2>
     </div>
+    <div class="paragraph">
+      <h1>Erklärung</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione suscipit animi expedita aliquam incidunt veniam, soluta quibusdam dolor
+        <a
+          href="#"
+        >Hallo klick mich an</a> dignissimos quisquam, ex quo provident vero at commodi? Perferendis velit ducimus maxime! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil porro ipsum commodi provident deleniti eaque quasi iusto adipisci, molestias et delectus laborum quidem eius sunt molestiae autem harum impedit nemo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ut vel cum dolorum porro, esse labore possimus? Reiciendis sit, quae explicabo repudiandae expedita maiores, accusamus, velit autem quas facilis illo?
+      </p>
+    </div>
+    <olap-filter title="Test" :active="true" />
+  </div>
 </template>
 
 <script>
+import OlapFilter from "../components/Filter.vue";
 export default {
-    components: {
-        "olap-filter": import('../components/Filter')
-    }
-}
+  components: {
+    "olap-filter": OlapFilter
+  }
+};
 </script>
