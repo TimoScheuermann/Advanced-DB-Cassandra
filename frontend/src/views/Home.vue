d499cf4033a9548f483d8f7a9dd0f43af44218c8
Timo Scheuermann <timo.scheuermann@gmx.de>
2019-10-11 10:16:26 +0200

    Added J Einleitung

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,17 +5,19 @@
       <h2>Cassandra</h2>
     </div>
     <div class="paragraph">
-      <h1>Erklärung</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione suscipit animi expedita aliquam incidunt veniam, soluta quibusdam dolor
-        <a
-          href="#"
-        >Hallo klick mich an</a> dignissimos quisquam, ex quo provident vero at commodi? Perferendis velit ducimus maxime! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil porro ipsum commodi provident deleniti eaque quasi iusto adipisci, molestias et delectus laborum quidem eius sunt molestiae autem harum impedit nemo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ut vel cum dolorum porro, esse labore possimus? Reiciendis sit, quae explicabo repudiandae expedita maiores, accusamus, velit autem quas facilis illo?
-      </p>
-      <div class="splitter"></div>
-      <h3>Moin Servus Moin</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laudantium dolorum esse nisi, aliquid nostrum sint consequatur, sapiente amet in quasi ut? Perspiciatis soluta, incidunt alias atque molestias voluptates quia?</p>
-    </div>
+
+<h2>NHL Statistics via Apache Cassandra</h2>
+		<p>	Im Rahmen des Wahlfaches "Other Database Systems" bei Prof. Dr. Julian Reichwald galt es, verschiedene Datenbanksysteme in einem Anwendungsfall vorzustellen und zu bewerten.
+			Diese Demoseite beschäftigt sich mit der In-memory columnstore Datenbank Cassandra von Apache.
+			Da sich für eine solche Datenbank ein Anwendungsfall mit vielen Datensätzen und Abhängigkeiten anbietet, enschieden wir uns für die Darstellung von NHL Statistiken mit Hilfe eines OLAP-Cubes.
+			Auf den folgenden Seiten lässt sich eine Demo ausführen, die die erziehlten Tore verschieder Spieler und Mannschaften, unter unterschiedlichen Gesichtspunkten auslesen lässt. Außerdem wird die Datenbank Cassandra, die OLAP-Operationen und der Programmcode vorgestellt.</p>
+
+	<h3>Wie die Demo funktioniert</h3>
+		<p>	Unter dem Reiter "Demo" gelangt man zu der Anzahl der gesamten erzielten Tore der NHL Saisons seit 2010. 
+			Nun gibt es die Möglichkeit verschiedene Filter auszuwählen, wie beispielsweise die Mannschaft, Spieler oder die Saison. 
+			Wählt man nun einen oder mehrere Filter aus, werden die Anzahl der Tore der Mannschaften oder Spieler in einer Tabelle angezeigt und die Gesamtzahl angepasst.</p>
+
+  </div>
   </div>
 </template>
 
